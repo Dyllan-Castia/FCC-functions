c9c13467c64d26932734b05c104b1aa37d1ed5df
Dyllan Castia <castiadyllan@gmail.com>
2025-11-21 18:09:32 -0800

    Fix username extraction and add alternative function
    These two functions both return the name of the email and exclude @gmail.com (or any other @mail.com) using different methods to achieve the same result.

--- username.js
+++ username.js
@@ -1,4 +1,15 @@
 let email = "kingkoopa@gmail.com";
 
 function getUserName(email) {
-  const divide = email.split[name, domain];
+  const [name, domain] = email.split("@");
+  const user = name.slice(0);
+  return user;
+}
+console.log(getUserName(email));
+
+function getNameAlt(email) {
+  const atIndex = email.indexOf("@");
+  const user = email.slice(0, atIndex);
+  return user;
+}
+console.log(getNameAlt(email));
